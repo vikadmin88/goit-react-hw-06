415527d6d1c3ad540d2852057a87295316696dba
Viktor Kudryavtsev <viktork8888@gmail.com>
2024-03-28 22:10:58 +0200

    fix

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,5 @@
 import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
-
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import { contactsReducer } from './contactsSlice';
 import { filterReducer } from './filtersSlice';
